4a4dc1be7d09a5faed8b29d0b78f1d8f4e9708a9
Bridget Shannon <bridgetrshannon@gmail.com>
2021-01-06 15:16:44 -0500

    added 60 second countdown timer with start and restart buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Circle Animation / Timing
 // variables that target container class and text id
 const container = document.querySelector(".container");
 const text = document.querySelector("#text");
@@ -30,3 +31,32 @@ function breatheAnimation() {
 
 // calls function every x amount of seconds the totalTime variable is for
 setInterval(breatheAnimation, totalTime);
+
+// Timer
+// event listener that targets the start button for the timer
+document.addEventListener("DOMContentLoaded", () => {
+  // variables that target ids in html
+  const timeLeftDisplay = document.querySelector("#time-left");
+  const startBtn = document.querySelector("#start-button");
+  const restartBtn = document.querySelector("#restart-button");
+  let timeLeft = 60;
+
+  function countDown() {
+    setInterval(function () {
+      if (timeLeft <= 0) {
+        clearInterval((timeLeft = 0));
+      }
+      timeLeftDisplay.innerHTML = timeLeft;
+      timeLeft -= 1;
+    }, 1000);
+  }
+
+  function restart() {
+    clearInterval((timeLeft = 60));
+  }
+  // on click event for start button that calls countDown function
+  startBtn.addEventListener("click", countDown);
+
+  // on click event for restart button that calls restart function
+  restartBtn.addEventListener("click", restart);
+});
